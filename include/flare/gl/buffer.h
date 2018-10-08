89252fd0c247f14fcbbef60ef930955d18ed73a0
velocic <velocicdev@gmail.com>
2018-10-07 23:42:34 -0400

    Add buffer & mapping functions to OpenGL buffer wrapper class

--- include/flare/gl/buffer.h
+++ include/flare/gl/buffer.h
@@ -1,12 +1,15 @@
 #ifndef FLARE_GL_BUFFER_H
 #define FLARE_GL_BUFFER_H
 
+#include <memory>
 #include <GL/gl3w.h>
 
 namespace Flare
 {
     namespace GL
     {
+        class MappedBufferRange;
+
         class Buffer
         {
             private:
@@ -20,10 +23,14 @@ namespace Flare
                     bool clientStorage = false;
                 };
 
+                UsageFlags usageFlags;
                 GLuint glBuffer = 0;
                 GLsizei dataCapacityBytes = 0;
+                std::unique_ptr<MappedBufferRange> mappedBuffer;
+                bool isCurrentlyMapped = false;
                 bool isGPUStorageInitialized = false;
-                UsageFlags usageFlags;
+
+                void checkDynamicStorageFlagBeforeWrite();
             public:
                 Buffer();
                 ~Buffer();
@@ -35,6 +42,9 @@ namespace Flare
                 void bind(GLenum target);
                 void destroy();
                 UsageFlags getUsageFlags() const;
+                MappedBufferRange* mapNamedBufferRange(GLintptr offset, GLsizeiptr length);
+                void namedBufferData(GLsizei size, const void* data, GLenum usage);
+                void namedBufferSubData(GLintptr offset, GLsizeiptr size, const void* data);
 
                 /*Flags:
                  * GL_DYNAMIC_STORAGE_BIT - Buffer contents can be updated directly
@@ -44,6 +54,33 @@ namespace Flare
                  * GL_MAP_COHERENT_BIT - Buffer maps are to be coherent
                  * GL_CLIENT_STORAGE_BIT - If all other conditions can be met, prefer CPU storage over GPU storage*/ 
                 void namedBufferStorage(GLsizei size, const void* data, GLbitfield flags);
+                void unmap();
+        };
+
+        class MappedBufferRange
+        {
+            friend class Buffer;
+            private:
+                const Buffer& sourceBuffer;
+                GLintptr offset = 0;
+                GLsizeiptr length = 0;
+                bool valid = true;
+
+                MappedBufferRange(const Buffer& sourceBuffer, GLintptr offset, GLsizeiptr length, void* mappedData);
+            public:
+                void* mappedData = nullptr;
+
+                ~MappedBufferRange();
+                MappedBufferRange(MappedBufferRange&& other) = delete;
+                MappedBufferRange& operator=(MappedBufferRange&& other) = delete;
+                MappedBufferRange(const MappedBufferRange& other) = delete;
+                MappedBufferRange& operator=(const Buffer& other) = delete;
+
+                bool hasReadAccess();
+                bool hasWriteAccess();
+                bool isPersistent();
+                bool isCoherent();
+                bool isValid();
         };
     }
 }
--- src/flare/gl/buffer.cpp
+++ src/flare/gl/buffer.cpp
@@ -1,3 +1,6 @@
+#include <stdexcept>
+#include <string>
+
 #include <flare/gl/buffer.h>
 
 namespace Flare
@@ -15,11 +18,41 @@ namespace Flare
         }
 
         Buffer::Buffer(Buffer&& other)
+        :
+            usageFlags(std::move(other.usageFlags)),
+            glBuffer(std::move(other.glBuffer)),
+            dataCapacityBytes(std::move(other.dataCapacityBytes)),
+            isGPUStorageInitialized(std::move(other.isGPUStorageInitialized))
         {
+            other.glBuffer = 0;
+            other.dataCapacityBytes = 0;
+            other.isGPUStorageInitialized = false;
+            other.usageFlags.dynamicStorage = false;
+            other.usageFlags.mapRead = false;
+            other.usageFlags.mapWrite = false;
+            other.usageFlags.mapPersistent = false;
+            other.usageFlags.mapCoherent = false;
+            other.usageFlags.clientStorage = false;
         }
 
         Buffer& Buffer::operator=(Buffer&& other)
         {
+            glBuffer = std::move(other.glBuffer);
+            dataCapacityBytes = std::move(other.dataCapacityBytes);
+            isGPUStorageInitialized = std::move(other.isGPUStorageInitialized);
+            usageFlags = std::move(other.usageFlags);
+
+            other.glBuffer = 0;
+            other.dataCapacityBytes = 0;
+            other.isGPUStorageInitialized = false;
+            other.usageFlags.dynamicStorage = false;
+            other.usageFlags.mapRead = false;
+            other.usageFlags.mapWrite = false;
+            other.usageFlags.mapPersistent = false;
+            other.usageFlags.mapCoherent = false;
+            other.usageFlags.clientStorage = false;
+
+            return *this;
         }
 
         void Buffer::bind(GLenum target)
@@ -35,15 +68,55 @@ namespace Flare
             usageFlags = UsageFlags{};
         }
 
+        MappedBufferRange* Buffer::mapNamedBufferRange(GLintptr offset, GLsizeiptr length)
+        {
+            if (isCurrentlyMapped) {
+                std::string message = "Mapping buffer " + std::to_string(glBuffer) + " while it "
+                    "is already mapped is not allowed. Unmap the buffer first";
+                throw std::runtime_error(message);
+
+                return nullptr;
+            }
+
+            GLbitfield mapFlags = 0;
+            if (usageFlags.mapRead) { mapFlags |= GL_MAP_READ_BIT; }
+            if (usageFlags.mapWrite) { mapFlags |= GL_MAP_WRITE_BIT; }
+            if (usageFlags.mapPersistent) { mapFlags |= GL_MAP_PERSISTENT_BIT; }
+            if (usageFlags.mapCoherent) { mapFlags |= GL_MAP_COHERENT_BIT; }
+
+            auto mappedData = glMapNamedBufferRange(glBuffer, offset, length, mapFlags);
+            mappedBuffer = std::unique_ptr<MappedBufferRange>(new MappedBufferRange(*this, offset, length, mappedData));
+
+            isCurrentlyMapped = true;
+
+            return mappedBuffer.get();
+        }
+
         Buffer::UsageFlags Buffer::getUsageFlags() const
         {
             return usageFlags;
         }
 
+        void Buffer::namedBufferData(GLsizei size, const void* data, GLenum usage)
+        {
+            checkDynamicStorageFlagBeforeWrite();
+            glNamedBufferData(glBuffer, size, data, usage);
+        }
+
+        void Buffer::namedBufferSubData(GLintptr offset, GLsizeiptr size, const void* data)
+        {
+            checkDynamicStorageFlagBeforeWrite();
+            glNamedBufferSubData(glBuffer, offset, size, data);
+        }
+
         void Buffer::namedBufferStorage(GLsizei size, const void* data, GLbitfield flags)
         {
             if (isGPUStorageInitialized) {
-                //TODO: figure out tests for error cases
+                auto message = "Allocating storage for buffer " + std::to_string(static_cast<unsigned int>(glBuffer)) +
+                    " which has already had storage allocated previously. Create a new buffer or explicitly destroy"
+                    " the storage before allocating.";
+                
+                throw std::runtime_error(message);
                 return;
             }
 
@@ -59,6 +132,62 @@ namespace Flare
                 flags & GL_MAP_COHERENT_BIT,
                 flags & GL_CLIENT_STORAGE_BIT
             };
+        }
+
+        void Buffer::unmap()
+        {
+            if (!isCurrentlyMapped) {
+                return;
+            }
+
+            glUnmapNamedBuffer(glBuffer);
+
+            //Leave the mapped buffer marked invalid incase anything monitoring it
+            //needs to update. It will be destroyed completely at the next call to
+            //mapNamedBufferRange();
+            mappedBuffer->valid = false;
+        }
+
+        void Buffer::checkDynamicStorageFlagBeforeWrite()
+        {
+            if (usageFlags.dynamicStorage == false) {
+                throw std::domain_error("Attempting to write data to buffer " + std::to_string(static_cast<unsigned int>(glBuffer))
+                    + " which is not marked for dynamic storage."
+                );
+            }
+        }
+
+        MappedBufferRange::MappedBufferRange(const Buffer& sourceBuffer, GLintptr offset, GLsizeiptr length, void* mappedData)
+        :
+            sourceBuffer(sourceBuffer),
+            offset(offset),
+            length(length),
+            mappedData(mappedData)
+        {}
+
+        MappedBufferRange::~MappedBufferRange()
+        {
+            valid = false;
+        }
+
+        bool MappedBufferRange::hasReadAccess()
+        {
+            return sourceBuffer.getUsageFlags().mapRead;
+        }
+
+        bool MappedBufferRange::hasWriteAccess()
+        {
+            return sourceBuffer.getUsageFlags().mapWrite;
+        }
+
+        bool MappedBufferRange::isPersistent()
+        {
+            return sourceBuffer.getUsageFlags().mapPersistent;
+        }
+
+        bool MappedBufferRange::isCoherent()
+        {
+            return sourceBuffer.getUsageFlags().mapCoherent;
         }
     }
 }
