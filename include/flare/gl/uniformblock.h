4e8554de095b733991e4fc1e21acf8066366ef16
velocic <velocicdev@gmail.com>
2019-04-26 23:34:38 -0400

    Cleaning up uniformblock

--- include/flare/gl/uniformblock.h
+++ include/flare/gl/uniformblock.h
@@ -8,6 +8,7 @@
 #include <utility>
 
 #include <glm-0.9.9/glm.hpp>
+#include <flare/gl/uniformblock_priv.h>
 
 namespace Flare
 {
@@ -48,99 +49,17 @@ namespace Flare
             using type = glm::mat4;
         };
 
-        template<size_t TotalBlockSize, typename Last>
-        constexpr size_t calculateUniformBlockSize(Last last)
-        {
-            if constexpr (TotalBlockSize % Last::alignment == 0) {
-                return Last::size + TotalBlockSize;
-            }
-
-            return TotalBlockSize + Last::size + (Last::alignment - (TotalBlockSize % Last::alignment));
-        }
-
-        template<size_t TotalBlockSize = 0, typename Current, typename... Remaining>
-        constexpr size_t calculateUniformBlockSize(Current current, Remaining... remaining)
-        {
-            if constexpr (TotalBlockSize % Current::alignment == 0) {
-                constexpr auto currentSize = Current::size + TotalBlockSize;
-                return calculateUniformBlockSize<currentSize>(remaining...);
-            }
-
-            constexpr auto paddedCurrentSize = TotalBlockSize + Current::size + (Current::alignment - (TotalBlockSize % Current::alignment));
-
-            return calculateUniformBlockSize<paddedCurrentSize>(remaining...);
-        }
-
-        template<size_t CurrentElementIndex, typename Last>
-        constexpr auto calculateUniformBlockAlignedElements(Last last)
-        {
-            if constexpr (CurrentElementIndex % Last::alignment == 0) {
-                return std::make_tuple(CurrentElementIndex);
-            }
-
-            constexpr auto alignmentPadding = Last::alignment - (CurrentElementIndex % Last::alignment);
-
-            return std::make_tuple(CurrentElementIndex + alignmentPadding);
-        }
-
-        template<size_t CurrentElementIndex = 0, typename Current, typename... Remaining>
-        constexpr auto calculateUniformBlockAlignedElements(Current current, Remaining... remaining)
-        {
-            if constexpr (CurrentElementIndex % Current::alignment == 0) {
-                constexpr auto nextElementCandidateIndex = CurrentElementIndex + Current::size;
-                return std::tuple_cat(std::make_tuple(CurrentElementIndex), calculateUniformBlockAlignedElements<nextElementCandidateIndex>(remaining...));
-            }
-
-            constexpr auto alignmentPadding = Current::alignment - (CurrentElementIndex % Current::alignment);
-            constexpr auto currentElementAlignedIndex = CurrentElementIndex + alignmentPadding;
-            constexpr auto nextElementCandidateIndex = currentElementAlignedIndex + Current::size;
-
-            return std::tuple_cat(std::make_tuple(currentElementAlignedIndex), calculateUniformBlockAlignedElements<nextElementCandidateIndex>(remaining...));
-        }
-
-        template<typename GLSLType>
-        constexpr auto buildEmptyUniformBlockElementPointerTuple(GLSLType last)
-        {
-            return std::tuple<typename GLSLType::type*>();
-        }
-
-        template<typename GLSLType, typename... GLSLTypes>
-        constexpr auto buildEmptyUniformBlockElementPointerTuple(GLSLType current, GLSLTypes... rest)
-        {
-            return std::tuple_cat(std::tuple<typename GLSLType::type*>(), buildEmptyUniformBlockElementPointerTuple(rest...));
-        }
-
-        template<typename... Pointers, typename... BufferOffsets>
-        constexpr auto assignPointersIntoBuffer(const std::tuple<Pointers...> &pointerTuple, const std::tuple<BufferOffsets...> &bufferOffsetsTuple, uint8_t *buffer)
-        {
-            if constexpr (sizeof...(Pointers) != sizeof...(BufferOffsets)) {
-                return pointerTuple;
-            }
-
-            return std::apply(
-                [&](const auto... pointers) {
-                    return std::apply(
-                        [&](const auto... bufferOffsets) {
-                            return std::tuple(reinterpret_cast<decltype(pointers)>(&buffer[bufferOffsets])...);
-                        },
-                        bufferOffsetsTuple
-                    );
-                },
-                pointerTuple
-            );
-        }
-
         template<typename... GLSLTypes>
-        constexpr auto buildAlignedUniformBlockBuffer(GLSLTypes... args)
+        constexpr auto buildStd140AlignedUniformBlockBuffer(GLSLTypes... args)
         {
-            constexpr auto bufferSize = calculateUniformBlockSize(args...);
-            constexpr auto alignedElementIndices = calculateUniformBlockAlignedElements(args...);
+            constexpr auto bufferSize = HelperTemplates::calculateUniformBlockSize(args...);
+            constexpr auto alignedElementIndices = HelperTemplates::calculateUniformBlockAlignedElements(args...);
 
             auto buffer = std::make_unique<uint8_t[]>(bufferSize);
 
-            constexpr decltype(buildEmptyUniformBlockElementPointerTuple(args...)) destinationPointerTuple{};
+            constexpr decltype(HelperTemplates::buildEmptyUniformBlockElementPointerTuple(args...)) destinationPointerTuple{};
 
-            return std::tuple_cat(std::make_tuple(std::move(buffer)), assignPointersIntoBuffer(destinationPointerTuple, alignedElementIndices, buffer.get()));
+            return std::tuple_cat(std::make_tuple(std::move(buffer)), HelperTemplates::assignPointersIntoBuffer(destinationPointerTuple, alignedElementIndices, buffer.get()));
         }
     }
 }
