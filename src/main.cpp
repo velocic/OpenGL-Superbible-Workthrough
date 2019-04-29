c23dc9c56e2ea61f143cb867342ebf057d751958
velocic <velocicdev@gmail.com>
2019-04-29 12:08:17 -0400

    Remove debug code for testing uniform block buffer init

--- src/main.cpp
+++ src/main.cpp
@@ -19,60 +19,6 @@ int main(int argc, char* argv[])
 
     Flare::Application* demoApp = &selectedTutorial;
 
-    //DEBUG
-    // auto&& [testBuffer, testFloat, testVec3, testMat4, testFloatArray3, testBool, testInt] = Flare::GL::buildStd140AlignedUniformBlockBuffer(
-    //     Flare::GL::GLSLType<float>{},
-    //     Flare::GL::GLSLType<glm::vec3>{},
-    //     Flare::GL::GLSLType<glm::mat4>{},
-    //     Flare::GL::GLSLArrayType<float, 3>{},
-    //     Flare::GL::GLSLType<bool>{},
-    //     Flare::GL::GLSLType<int>{}
-    // );
-
-    // auto testBlock = UniformBlock(
-    //     GLSLType<float>{},
-    //     GLSLType<glm::vec3>{},
-    //     GLSLType<glm::mat4>{},
-    //     GLSLArrayType<float, 3>{},
-    //     GLSLType<bool>{},
-    //     GLSLType<int>{}
-    // );
-    auto testBlock = Flare::GL::buildStd140AlignedUniformBlockBuffer(
-        Flare::GL::GLSLType<float>{},
-        Flare::GL::GLSLType<glm::vec3>{},
-        Flare::GL::GLSLType<glm::mat4>{},
-        Flare::GL::GLSLArrayType<float, 3>{},
-        Flare::GL::GLSLType<bool>{},
-        Flare::GL::GLSLType<int>{}
-    );
-
-    auto&& [testBuffer, testFloat, testVec3, testMat4, testFloatArray3, testBool, testInt] = testBlock;
-    //
-    *testFloat = 32.5f;
-    *testVec3 = glm::vec3(-25, 25, 125);
-    *testMat4 = glm::mat4(
-        10, 10, 10, 10,
-        20, 20, 20, 20,
-        30, 30, 30, 30,
-        40, 40, 40, 40
-    );
-    testFloatArray3[0] = 25.0f;
-    testFloatArray3[1] = 50.2f;
-    testFloatArray3[2] = 75.5f;
-    *testBool = true;
-    *testInt = ~0;
-
-    // auto testBlock = Flare::GL::buildStd140AlignedUniformBlockBuffer(
-    //     Flare::GL::GLSLType<float>{},
-    //     Flare::GL::GLSLType<glm::vec3>{},
-    //     Flare::GL::GLSLType<glm::mat4>{},
-    //     Flare::GL::GLSLArrayType<float, 3>{},
-    //     Flare::GL::GLSLType<bool>{},
-    //     Flare::GL::GLSLType<int>{}
-    // );
-    int debug = 5;
-    //END DEBUG
-
     demoApp->initialize();
 
     //Fake a render loop at slow enough FPS to not blind myself
