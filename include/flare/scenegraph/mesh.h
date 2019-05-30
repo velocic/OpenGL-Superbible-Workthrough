75b30431043c17f6119b2e82ac2bb1cab9bc949e
velocic <velocicdev@gmail.com>
2019-05-30 13:38:33 -0400

    Designing mesh class; Realized it needs a RenderSystem::VertexArray interface that doesn't exist yet; Will need a MeshManager, too.

--- include/flare/scenegraph/mesh.h
+++ include/flare/scenegraph/mesh.h
@@ -2,13 +2,35 @@
 #define FLARE_SCENEGRAPH_MESH_H
 
 #include <string>
+#include <vector>
 
 #include <glm-0.9.9/glm.hpp>
+
+#include <flare/rendersystem/texture.h>
+#include <flare/utility/datatypes.h>
 
 namespace Flare
 {
     namespace SceneGraph
     {
+        class Mesh
+        {
+            private:
+                std::vector<DataTypes::Vertex> vertices;
+                std::vector<unsigned int> indices;
+                std::vector<RenderSystem::Texture *> textures;
+                RenderSystem::VertexArray VAO;
+                RenderSystem::Buffer VBO;
+                RenderSystem::Buffer EBO;
+
+            public:
+                Mesh(std::vector<DataTypes::Vertex> vertices, std::vector<unsigned int> indices, std::vector<RenderSystem::Texture *> textures);
+                ~Mesh();
+                Mesh(Mesh &&other);
+                Mesh &operator=(Mesh &&other);
+                Mesh(const Mesh &other) = delete;
+                Mesh &operator=(const Mesh &other) = delete;
+        };
     }
 }
 
--- include/flare/utility/datatypes.h
+++ include/flare/utility/datatypes.h
@@ -1,10 +1,18 @@
 #ifndef FLARE_UTILITY_DATATYPES_H
 #define FLARE_UTILITY_DATATYPES_H
 
+#include <glm-0.9.9/glm.hpp>
+
 namespace Flare
 {
     namespace DataTypes
     {
+        struct Vertex
+        {
+            glm::vec3 position;
+            glm::vec3 normal;
+            glm::vec2 uvCoords;
+        };
     }
 }
 
