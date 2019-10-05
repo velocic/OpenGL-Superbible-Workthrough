f90033deb0d6690a0c16de11278d28c0e60c211e
velocic <velocicdev@gmail.com>
2019-10-05 18:43:46 -0400

    Add support for DataLayout to describe matrix attributes (which span multiple glsl layout locations)

--- include/flare/rendersystem/datalayout.h
+++ include/flare/rendersystem/datalayout.h
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <string>
 #include <vector>
+#include <variant>
 
 #include <flare/rendersystem/datatypes.h>
 
@@ -21,12 +22,23 @@ namespace Flare
             RSuint attribDivisor = 0;
         };
 
+        struct MatrixVertexAttribute
+        {
+            std::string name;
+            RSint rows = 0;
+            RSint cols = 0;
+            RSenum type;
+            RSboolean normalized = GL_FALSE;
+            RSuint relativeOffset = 0;
+            RSuint attribDivisor = 0;
+        };
+
         struct VertexDataLayout
         {
             friend class VertexDataLayoutBuilder;
 
             private:
-                VertexDataLayout(RSsizei stride, RSintptr offset, std::vector<VertexAttribute> &&vertexAttributes)
+                VertexDataLayout(RSsizei stride, RSintptr offset, std::vector<std::variant<VertexAttribute, MatrixVertexAttribute>> &&vertexAttributes)
                 : stride(stride), offset(offset), vertexAttributes(vertexAttributes)
                 {
                 }
@@ -37,15 +49,17 @@ namespace Flare
                 const RSsizei stride = 0;
                 //always set to zero unless interleaving data blocks within a single buffer
                 const RSintptr offset = 0;
-                const std::vector<VertexAttribute> vertexAttributes;
+                const std::vector<std::variant<VertexAttribute, MatrixVertexAttribute>> vertexAttributes;
 
-                const VertexAttribute *getAttribute(const std::string &attributeName)
+                const std::variant<VertexAttribute, MatrixVertexAttribute> *getAttribute(const std::string &attributeName)
                 {
                     const auto& result = std::find_if(
                         vertexAttributes.begin(),
                         vertexAttributes.end(),
-                        [attributeName](const auto& entry) {
-                            return entry.name == attributeName;
+                        [&attributeName](const auto& entry) {
+                            auto findResult = false;
+                            std::visit([&attributeName, &findResult](const auto &vertexAttribute){ findResult = vertexAttribute.name == attributeName; }, entry);
+                            return findResult;
                         }
                     );
 
@@ -60,7 +74,7 @@ namespace Flare
         class VertexDataLayoutBuilder
         {
             private:
-                std::vector<VertexAttribute> vertexAttributes;
+                std::vector<std::variant<VertexAttribute, MatrixVertexAttribute>> vertexAttributes;
                 RSsizei stride = 0;
                 RSintptr offset = 0;
             public:
@@ -73,6 +87,18 @@ namespace Flare
                 VertexDataLayoutBuilder &addAttribute(const std::string &attributeName, RSint size, RSenum type, RSboolean normalized, RSuint relativeOffset, RSuint attribDivisor)
                 {
                     vertexAttributes.push_back(VertexAttribute{attributeName, size, type, normalized, relativeOffset, attribDivisor});
+                    return *this;
+                }
+
+                VertexDataLayoutBuilder &addMatrixAttribute(const std::string &attributeName, RSint rows, RSint cols, RSenum type, RSboolean normalized, RSuint relativeOffset)
+                {
+                    vertexAttributes.push_back(MatrixVertexAttribute{attributeName, rows, cols, type, normalized, relativeOffset, 0});
+                    return *this;
+                }
+
+                VertexDataLayoutBuilder &addMatrixAttribute(const std::string &attributeName, RSint rows, RSint cols, RSenum type, RSboolean normalized, RSuint relativeOffset, RSuint attribDivisor)
+                {
+                    vertexAttributes.push_back(MatrixVertexAttribute{attributeName, rows, cols, type, normalized, relativeOffset, attribDivisor});
                     return *this;
                 }
 
@@ -91,7 +117,7 @@ namespace Flare
                 VertexDataLayout build()
                 {
                     VertexDataLayout result = VertexDataLayout(stride, offset, std::move(vertexAttributes));
-                    vertexAttributes = std::vector<VertexAttribute>();
+                    vertexAttributes = std::vector<std::variant<VertexAttribute, MatrixVertexAttribute>>();
                     stride = 0;
                     offset = 0;
 
