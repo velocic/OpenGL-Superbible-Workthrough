0347588c4dcf5a15bf4dee24ba4f6f420b595572
velocic <velocicdev@gmail.com>
2019-10-09 22:54:18 -0400

    prevent nvidia optimizing out shader args

--- src/7b-packed-mesh-rendering/shaders/fragment.glsl
+++ src/7b-packed-mesh-rendering/shaders/fragment.glsl
@@ -15,6 +15,8 @@ layout (binding = 3) uniform sampler2D textureNormal0;
 
 void main(void)
 {
-    // color = fs_in.color - fs_in.color + texture(textureDiffuse0, fs_in.uvCoord);
-    color = texture(textureDiffuse0, fs_in.uvCoord);
+    //color = fs_in.color - fs_in.color + texture(textureDiffuse0, fs_in.uvCoord);
+    //color = texture(textureDiffuse0, fs_in.uvCoord);
+    //Prevent params optimizing out on Nvidia drivers for testing
+    color = (fs_in.color + vec4(1, 0, 0, 1)) + texture(textureDiffuse0, fs_in.uvCoord) - fs_in.color - vec4(1, 0, 0, 1);
 }
