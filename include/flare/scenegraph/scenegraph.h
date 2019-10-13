c0435a4194b25a9fd9e57e05aa090b4e4c132a6f
velocic <velocicdev@gmail.com>
2019-10-13 10:35:38 -0400

    Merge scenegraph & node files; fixes circular include, and they're tightly coupled classes anyway

--- include/flare/scenegraph/scenegraph.h
+++ include/flare/scenegraph/scenegraph.h
@@ -1,7 +1,11 @@
 #ifndef FLARE_SCENEGRAPH_SCENEGRAPH_H
 #define FLARE_SCENEGRAPH_SCENEGRAPH_H
 
-#include <flare/scenegraph/node.h>
+#include <flare/rendersystem/buffermanager.h>
+#include <flare/scenegraph/model.h>
+
+#include <glm-0.9.9/glm.hpp>
+#include <glm-0.9.9/gtc/matrix_transform.hpp>
 
 namespace Flare
 {
@@ -19,6 +23,66 @@ namespace Flare
                 Node *createNode(Node *parent, Model *model);
                 Node *createNode(Node *parent, size_t instanceCountReserveSize);
                 Node *createNode(Node *parent, size_t instanceCountReserveSize, Model *model);
+        };
+
+        class Node
+        {
+            friend class SceneGraph;
+            private:
+                struct InstanceData
+                {
+                    std::vector<glm::mat4> worldMatrices;
+                    size_t numActive = 0;
+                };
+
+                InstanceData instanceData;
+                std::vector<Node *> children;
+                glm::vec3 translation;
+                glm::vec3 rotation;
+                glm::vec3 scale;
+                size_t name = 0;
+                SceneGraph &sceneGraph;
+                RenderSystem::BufferManager &bufferManager;
+                RenderSystem::Buffer *mvpMatrixBuffer = nullptr;
+                Node *parent = nullptr;
+                Model *model = nullptr;
+
+                Node(SceneGraph &sceneGraph, RenderSystem::BufferManager &bufferManager, Node *parent);
+                Node(SceneGraph &sceneGraph, RenderSystem::BufferManager &bufferManager, Node *parent, Model *model);
+                Node(SceneGraph &sceneGraph, RenderSystem::BufferManager &bufferManager, Node *parent, size_t instanceCountReserveSize);
+                Node(SceneGraph &sceneGraph, RenderSystem::BufferManager &bufferManager, Node *parent, size_t instanceCountReserveSize, Model *model);
+
+                void notifyChildReparented(Node *child);
+                RenderSystem::VertexDataLayout getMVPMatrixBufferLayout() const;
+            public:
+                ~Node();
+                Node(const Node &other);
+                Node(Node &&other);
+                Node &operator=(const Node &other);
+                Node &operator=(Node &&other);
+
+                void destroy();
+
+                size_t getName() const;
+                glm::mat4 getLocalTransform(size_t instanceId) const;
+
+                void setParent(Node *newParent);
+                void setModel(Model *newModel);
+
+                void translateNode(const glm::vec3 &translation);
+                void rotateNode(const glm::vec3 &rotation);
+                void scaleNode(const glm::vec3 &scale);
+
+                void translateInstance(size_t index, const glm::vec3 &translation);
+                void rotateInstance(size_t index, const glm::vec3 &rotation);
+                void scaleInstance(size_t index, const glm::vec3 &scale);
+
+                size_t addInstance();
+                void addChildNode(Node *child);
+                void removeInstance(size_t index);
+                void removeAllInstances();
+                void removeChildNode(Node *child);
+                void removeAllChildren();
         };
     }
 }
--- src/flare/scenegraph/scenegraph.cpp
+++ src/flare/scenegraph/scenegraph.cpp
@@ -8,5 +8,147 @@ namespace Flare
         {
             return nextNameToAssign++;
         }
+
+        Node::Node(SceneGraph &sceneGraph, RenderSystem::BufferManager &bufferManager, Node *parent)
+        :
+            sceneGraph(sceneGraph),
+            bufferManager(bufferManager),
+            parent(parent)
+        {
+        }
+
+        Node::Node(SceneGraph &sceneGraph, RenderSystem::BufferManager &bufferManager, Node *parent, Model *model)
+        :
+            sceneGraph(sceneGraph),
+            bufferManager(bufferManager),
+            parent(parent),
+            model(model)
+        {
+        }
+
+        Node::Node(SceneGraph &sceneGraph, RenderSystem::BufferManager &bufferManager, Node *parent, size_t instanceCountReserveSize)
+        :
+            sceneGraph(sceneGraph),
+            bufferManager(bufferManager),
+            parent(parent)
+        {
+            instanceData.worldMatrices.resize(instanceCountReserveSize);
+        }
+
+        Node::Node(SceneGraph &sceneGraph, RenderSystem::BufferManager &bufferManager, Node *parent, size_t instanceCountReserveSize, Model *model)
+        :
+            sceneGraph(sceneGraph),
+            bufferManager(bufferManager),
+            parent(parent),
+            model(model)
+        {
+            instanceData.worldMatrices.resize(instanceCountReserveSize);
+        }
+
+        Node::~Node()
+        {
+            destroy();
+        }
+
+        Node::Node(const Node &other)
+        :
+            instanceData(other.instanceData),
+            translation(translation),
+            rotation(rotation),
+            scale(scale),
+        {
+            //TODO: deep copy children
+            //TODO: request unique name from SceneGraph
+        }
+
+        Node::Node(Node &&other)
+        {
+        }
+
+        Node &Node::operator=(const Node &other)
+        {
+        }
+
+        Node &Node::operator=(Node &&other)
+        {
+        }
+
+        void Node::destroy()
+        {
+        }
+
+        size_t getName() const
+        {
+            return name;
+        }
+
+        glm::mat4 Node::getLocalTransform(size_t instanceId) const
+        {
+        }
+
+        void Node::setParent(Node *newParent)
+        {
+        }
+
+        void Node::setModel(Model *newModel)
+        {
+        }
+
+        void Node::translateNode(const glm::vec3 &translation)
+        {
+        }
+
+        void Node::rotateNode(const glm::vec3 &rotation)
+        {
+        }
+
+        void Node::scaleNode(const glm::vec3 &scale)
+        {
+        }
+
+        void Node::translateInstance(size_t index, const glm::vec3 &translation)
+        {
+        }
+
+        void Node::rotateInstance(size_t index, const glm::vec3 &rotation)
+        {
+        }
+
+        void Node::scaleInstance(size_t index, const glm::vec3 &scale)
+        {
+        }
+
+        size_t Node::addInstance()
+        {
+        }
+
+        void Node::addChildNode(Node *child)
+        {
+        }
+
+        void Node::removeInstance(size_t index)
+        {
+        }
+
+        void Node::removeAllInstances()
+        {
+        }
+
+        void Node::removeChildNode(Node *child)
+        {
+        }
+
+        void Node::removeAllChildren()
+        {
+        }
+
+        RenderSystem::VertexDataLayout Node::getMVPMatrixBufferLayout() const
+        {
+            return Flare::RenderSystem::VertexDataLayoutBuilder()
+                .addMatrixAttribute("mvpMatrix", 4, 4, Flare::RenderSystem::RS_FLOAT, Flare::RenderSystem::RS_FALSE, 0)
+                .setStride(sizeof(glm::mat4))
+                .setDivisor(1)
+                .build();
+        }
     }
 }
